b9f6d50503e0bf29169bebbbd3377cce43864f9b
Anshika Mishra <33021256+anshika1297@users.noreply.github.com>
2023-03-04 19:29:09 +0530

    ..

---
+++ src/js/typed.js
@@ -0,0 +1,11 @@
+import Typed from 'typed.js';
+
+var options = {
+  strings: ["I'm a Mobile App Developer", 'I Build Mobile Apps', 'I am an Avid Reader'],
+  typeSpeed: 120,
+  backSpeed: 50,
+  backDelay: 1500,
+  loop: true
+};
+
+var typed = new Typed('#headline', options);
